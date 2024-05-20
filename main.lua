dfe41b335dbb3b35e87ab29a7b80f6bd5095d93e
wutname1 <jeremy12@gmail.com>
2024-05-20 10:18:12 -0500

    Added new category and search items
    The update includes the addition of a new category title for transmog items. Also, more search items have been added to improve item identification. The filter function has been expanded to check for cosmetic learnable items and return the appropriate category title.

--- main.lua
+++ main.lua
@@ -12,11 +12,15 @@ end
 local Tooltip = CreateFrame('GameTooltip', 'BBOpenable', nil, 'GameTooltipTemplate')
 local OPENABLE_CATEGORY_TITLE = '|cff2beefd Openable'
 local LOCKBOXES_CATEGORY_TITLE = '|cff2beefd Lockboxes'
+local TRANSMOG_CATEGORY_TITLE = '|cff2beefd Lockboxes'
 
 local SearchItems = {
 	'Open the container',
 	'Use: Open',
-	'Right Click to Open'
+	ITEM_OPENABLE,
+	ITEM_CREATE_LOOT_SPEC_ITEM,
+	ITEM_SPELL_TRIGGER_ONUSE,
+	ITEM_TOY_ONUSE
 }
 
 ---@param data ItemData
@@ -33,12 +37,20 @@ local function filter(data)
 		line = _G['BBOpenableTextLeft' .. i]
 		local LineText = line:GetText()
 		Log(LineText)
+
 		--Search for the strings in the tooltip
 		for _, v in pairs(SearchItems) do
 			if string.find(LineText, v) then
 				return OPENABLE_CATEGORY_TITLE
 			end
 		end
+
+		-- Check for ITEM_COSMETIC_LEARN
+		if LineText == ITEM_COSMETIC_LEARN then
+			return TRANSMOG_CATEGORY_TITLE
+		end
+
+		-- Check for LOCKED
 		if LineText == LOCKED then
 			return LOCKBOXES_CATEGORY_TITLE
 		end
