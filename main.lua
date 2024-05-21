e186360ce6fa1a46a1d8bdb49766d8466f661b25
wutname1 <jeremy12@gmail.com>
2024-05-20 21:26:13 -0500

    Add Mounts & Localize some strings

--- main.lua
+++ main.lua
@@ -16,12 +16,37 @@ local profile = {
 	FilterGenericUse = false,
 	FilterToys = true,
 	FilterAppearance = true,
+	FilterMounts = true,
 	CreatableItem = true
 }
 
 --Setup DB
 local DataBase = LibStub('AceDB-3.0'):New('BetterBagsOpenableDB', {profile = profile}, true)
 local DB = DataBase.profile ---@type Profile
+
+--Get Locale
+local Localized = {
+	deDE = {
+		['Use: Teaches you how to summon this mount'] = 'Benutzen: Lehrt Euch, dieses Reittier herbeizurufen',
+		['Use: Collect the appearance'] = 'Benutzen: Sammelt das Aussehen'
+	},
+	esES = {
+		['Use: Teaches you how to summon this mount'] = 'Uso: Te enseña a invocar esta montura',
+		['Use: Collect the appearance'] = 'Uso: Recoge la apariencia'
+	},
+	frFR = {
+		['Use: Teaches you how to summon this mount'] = 'Utilisation: Vous apprend à invoquer cette monture',
+		['Use: Collect the appearance'] = "Utilisation: Collectionnez l'apparence"
+	}
+}
+
+local Locale = GetLocale()
+function GetLocaleString(key)
+	if Localized[Locale] then
+		return Localized[Locale][key]
+	end
+	return key
+end
 
 --Setup Options
 local options = {
@@ -49,6 +74,19 @@ local options = {
 		end,
 		set = function(_, value)
 			DB.FilterToys = value
+		end
+	},
+	FilterMounts = {
+		type = 'toggle',
+		width = 'full',
+		order = 1,
+		name = 'Filter Mounts',
+		desc = 'Filter all items with `' .. GetLocaleString('Use: Teaches you how to summon this mount') .. '` in the tooltip',
+		get = function()
+			return DB.FilterMounts
+		end,
+		set = function(_, value)
+			DB.FilterMounts = value
 		end
 	},
 	FilterAppearance = {
@@ -117,7 +155,7 @@ local function filter(data)
 			end
 		end
 
-		if DB.FilterAppearance and (string.find(LineText, ITEM_COSMETIC_LEARN) or string.find(LineText, 'Use: Collect the appearance')) then
+		if DB.FilterAppearance and (string.find(LineText, ITEM_COSMETIC_LEARN) or string.find(LineText, GetLocaleString('Use: Collect the appearance'))) then
 			return PREFIX .. 'Cosmetics'
 		end
 
@@ -133,6 +171,10 @@ local function filter(data)
 
 		if DB.FilterToys and string.find(LineText, ITEM_TOY_ONUSE) then
 			return PREFIX .. 'Toys'
+		end
+
+		if DB.FilterMounts and string.find(LineText, GetLocaleString('Use: Teaches you how to summon this mount')) then
+			return PREFIX .. 'Mounts'
 		end
 
 		if DB.FilterGenericUse and string.find(LineText, ITEM_SPELL_TRIGGER_ONUSE) then
